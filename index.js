fc40329f52b75a781c7199a80d7e0efa8fc16c13
Acsais <promo.acsais@yandex.ru>
2024-06-24 20:43:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on('message', async msg => {
 			const data = JSON.parse(msg?.web_app_data?.data)
 			console.log(data)
 			await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-			await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
+			await bot.sendMessage(chatId, 'Ваш населенный пункт: ' + data?.country)
 			await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
 
 			setTimeout(async () => {
